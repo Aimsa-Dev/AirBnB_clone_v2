7a35e615e3bd9d5770aa3df4184640a96272acbd
Aimsa-Dev <adeniyidawud@gmail.com>
2024-01-28 19:57:10 +0000

    task

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''A simple Flask web application.
+'''
+from flask import Flask
+
+
+app = Flask(__name__)
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    '''The home page.'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''The hbnb page.'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    '''The c page.'''
+    return 'C {}'.format(text.replace('_', ' '))
